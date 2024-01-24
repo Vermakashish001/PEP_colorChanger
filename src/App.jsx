8a769c4d90ebe368c1bb85578cba3ad50127cc9b
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-01-24 11:27:37 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import "./App.css"
+import Themechanger from './components/Themechanger';
+import Conditionalrendering from './components/Conditionalrendering';
+export default function App() {
+  const [condition,setCondition] = useState(true);
+  
+  function handleColor(){
+    setCondition(!condition)
+  }
+  return (
+    <>
+    <div className={condition?"active":"not-active"} style={{backgroundColor:condition?"white":"black",color:condition?"black":"white"}}>
+      <h1>Hello World!</h1>
+      <button onClick={()=>(handleColor())}>{condition?"Dark":"Light"}</button>
+      <Themechanger/>
+      {-10 && (
+        <h1>Hello Kashish</h1>
+      )}
+    </div>
+    <Conditionalrendering/>
+    </>
+    
+  )
+}
+
